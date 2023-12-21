fb369958d4723dd37a822361d881c4c4e31c3ca0
vakrilov <alexander.vakrilov@gmail.com>
2023-12-21 23:13:26 +0200

    configure vite proxy

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,30 @@
-import './App.css'
+import { useState } from "react";
+import "./App.css";
 
-function App() {
+// const BASE_URL = "https://5kmrun.bg/api/";
+const BASE_URL = "http://localhost:5173/api/";
+const USER_API = "selfie/user/";
+
+const getData = async (userID) => {
+  if (import.meta.env.MODE === "development") {
+    const response = await fetch(`${BASE_URL}${USER_API}${userID}`);
+    const data = await response.json();
+    console.log(data);
+  }
+};
+
+const App = () => {
+  const [userId, setUserId] = useState(null);
+
   return (
     <>
       <h1>5kmrun Wrapped</h1>
       <div className="card">
-        Dragons be here
+        <input type="text" placeholder="ID" onChange={(e) => setUserId(e.target.value)} />
+        <button onClick={() => getData(userId)}>Get</button>
       </div>
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,23 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+
+  server: {
+    // with options: http://localhost:5173/api/bar-> http://jsonplaceholder.typicode.com/bar
+    proxy: {
+      // Match the endpoint you want to proxy
+      "/api": {
+        // Target host
+        target: "https://5kmrun.bg/",
+        changeOrigin: true,
+        // rewrite: (path) => {
+        //   const result = path.replace(/^\/api/, "/api/5kmrun");
+        //   return result;
+        // },
+      },
+    },
+  },
+});
