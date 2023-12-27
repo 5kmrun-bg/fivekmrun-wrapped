67fc05f7df2cc19640bab2b49af35ebbc4d35d43
vakrilov <alexander.vakrilov@gmail.com>
2023-12-27 16:30:21 +0200

    test cors

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,34 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
 
-// const BASE_URL = "https://5kmrun.bg/api/";
-const BASE_URL = "http://localhost:5173/api/";
+const BASE_URL =
+  import.meta.env.MODE === "development"
+    ? "http://localhost:5173/api/"
+    : "https://5kmrun.bg/api/";
 const USER_API = "selfie/user/";
 
 const getData = async (userID) => {
-  if (import.meta.env.MODE === "development") {
-    const response = await fetch(`${BASE_URL}${USER_API}${userID}`);
-    const data = await response.json();
-    console.log(data);
-  }
+  const response = await fetch(`${BASE_URL}${USER_API}${userID}`);
+  const data = await response.json();
+  console.log(data);
 };
 
 const App = () => {
   const [userId, setUserId] = useState(null);
 
+  useEffect(() => {
+    getData("18880");
+  }, []);
+
   return (
     <>
       <h1>5kmrun Wrapped</h1>
       <div className="card">
-        <input type="text" placeholder="ID" onChange={(e) => setUserId(e.target.value)} />
+        <input
+          type="text"
+          placeholder="ID"
+          onChange={(e) => setUserId(e.target.value)}
+        />
         <button onClick={() => getData(userId)}>Get</button>
       </div>
     </>
