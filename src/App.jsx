d45ac645cacafdcec574fe6ddb6a4c46c8140610
vakrilov <alexander.vakrilov@gmail.com>
2023-12-30 16:00:21 +0200

    stats loading

--- src/App.jsx
+++ src/App.jsx
@@ -1,81 +1,17 @@
 import { useEffect, useState } from "react";
 import "./App.css";
-import { getData } from "./api";
-import _ from "lodash";
-
-const loadData = async (userID) => {
-  const data = await getData(userID);
-
-  console.log("PARSED DATA:");
-  console.log(data);
-
-  console.log("OFFICIAL RUNS Stats:");
-  console.log(getOfficialRunsStats(data));
-
-  console.log("SELFIE RUNS Stats:");
-  console.log(getSelfieRunsStats(data));
-
-  console.log("XL RUNS Stats:");
-  console.log(getXLRunsStats(data));
-};
-
-const getOfficialRunsStats = (data) => {
-  const { runs } = data;
-
-  if (!runs) {
-    return null;
-  }
-
-  return {
-    activeWeeks: runs.length,
-    totalDistance: _.sumBy(runs, "distance"),
-    totalTime: _.sumBy(runs, "time"),
-    fastestRun: _.minBy(runs, "time"),
-    bestPositionRun: _.minBy(runs, "position"),
-    locationBreakdown: _.chain(runs)
-      .countBy("location")
-      .toPairs()
-      .sortBy(1)
-      .reverse()
-      .value(),
-  };
-};
-
-const getSelfieRunsStats = (data) => {
-  const { selfieRuns } = data;
-
-  if (!selfieRuns) {
-    return null;
-  }
-
-  return {
-    activeWeeks: selfieRuns.length,
-    totalDistance: _.sumBy(selfieRuns, "distance"),
-    totalTime: _.sumBy(selfieRuns, "time"),
-    fastestRuns: _.chain(selfieRuns).sortBy("time").take(3).value(),
-    bestPositionRun: _.chain(selfieRuns).sortBy("position").take(3).value(),
-  };
-};
-
-const getXLRunsStats = (data) => {
-  const { xlRuns } = data;
-
-  if (!xlRuns) {
-    return null;
-  }
-
-  return {
-    numRaces: xlRuns.length,
-    totalDistance: _.sumBy(xlRuns, "distance"),
-    bestPositionRun: _.chain(xlRuns).sortBy("position").take(3).value(),
-  };
-}
+import { loadStats } from "./stats";
 
 const App = () => {
   const [userId, setUserId] = useState(null);
+  const [stats, setStats] = useState(null);
 
   useEffect(() => {
-    loadData("13731");
+    const load = async () => {
+      const stats = await loadStats("13731");
+      setStats(stats);
+    };
+    load();
   }, []);
 
   return (
@@ -87,7 +23,13 @@ const App = () => {
           placeholder="ID"
           onChange={(e) => setUserId(e.target.value)}
         />
-        <button onClick={() => loadData(userId)}>Get</button>
+        <button onClick={() => loadStats(userId)}>Get</button>
+
+        {stats ? (
+          <pre>{JSON.stringify(stats, null, 2)}</pre>
+        ) : (
+          <div> Loading ... </div>
+        )}
       </div>
     </>
   );
---
+++ src/stats/index.js
@@ -0,0 +1,68 @@
+import { getData } from "../api";
+import _ from "lodash";
+
+export const loadStats = async (userID) => {
+  const data = await getData(userID);
+
+  // console.log("PARSED DATA:");
+  // console.log(data);
+
+  return {
+    user: data.user,
+    officialRuns: getOfficialRunsStats(data),
+    selfieRuns: getSelfieRunsStats(data),
+    xlRuns: getXLRunsStats(data),
+  };
+};
+
+const getOfficialRunsStats = (data) => {
+  const { runs } = data;
+
+  if (!runs) {
+    return null;
+  }
+
+  return {
+    activeWeeks: runs.length,
+    totalDistance: _.sumBy(runs, "distance"),
+    totalTime: _.sumBy(runs, "time"),
+    fastestRun: _.minBy(runs, "time"),
+    bestPositionRun: _.minBy(runs, "position"),
+    locationBreakdown: _.chain(runs)
+      .countBy("location")
+      .toPairs()
+      .sortBy(1)
+      .reverse()
+      .value(),
+  };
+};
+
+const getSelfieRunsStats = (data) => {
+  const { selfieRuns } = data;
+
+  if (!selfieRuns) {
+    return null;
+  }
+
+  return {
+    activeWeeks: selfieRuns.length,
+    totalDistance: _.sumBy(selfieRuns, "distance"),
+    totalTime: _.sumBy(selfieRuns, "time"),
+    fastestRuns: _.chain(selfieRuns).sortBy("time").take(3).value(),
+    bestPositionRun: _.chain(selfieRuns).sortBy("position").take(3).value(),
+  };
+};
+
+const getXLRunsStats = (data) => {
+  const { xlRuns } = data;
+
+  if (!xlRuns) {
+    return null;
+  }
+
+  return {
+    numRaces: xlRuns.length,
+    totalDistance: _.sumBy(xlRuns, "distance"),
+    bestPositionRun: _.chain(xlRuns).sortBy("position").take(3).value(),
+  };
+};
