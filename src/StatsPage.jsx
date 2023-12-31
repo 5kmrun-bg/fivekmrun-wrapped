5907f5551d3b9ed4cd44af91c3bd5314e4b931e3
etabakov <emil.tabakov@gmail.com>
2023-12-31 18:06:01 +0200

    scaffolded screens with some dummy text and data

--- src/StatsPage.jsx
+++ src/StatsPage.jsx
@@ -6,53 +6,6 @@ import joroAvatar from "./assets/joro-avatar.png";
 const width = 360;
 const height = 640;
 
-const stories = [
-  {
-      content: (props) => (
-          <div style={{  backgroundImage: "url('https://5kmrun.bg/files/header_5_large.jpg')", backgroundColor: 'rgba(0,0,0,0.5)'}}>
-            <div style={{ backgroundColor: 'rgba(0,0,0,0.5)', padding: "20px"}}>
-              <h1 style={{ marginTop: 5 }}>2023-та с 5kmrun.bg</h1>
-              <div>
-                Отправяме специален поздрав за всеки един участник на 5kmrun през 2023 г. и желаем здраве и още повече пробягани километри през Новата година!
-              </div>
-              <img src={joroAvatar} />
-            </div>
-          </div>
-      ),
-      },
-      {
-      content: (props) => (
-        <div style={{  backgroundImage: "url('https://5kmrun.bg/files/header_2_large.jpg')", backgroundColor: 'rgba(0,0,0,0.5)'}}>
-          <div style={{ backgroundColor: 'rgba(0,0,0,0.5)', padding: "20px"}}>
-            <h1 style={{ marginTop: 5 }}>Същински бягания</h1>
-            <div>
-              През тази година участвахте в ... бягания.
-            </div>
-            <div>
-              Пробягахте общо ... километра.
-            </div>
-            <div>
-              И това ви отне общо ... часа.
-            </div>
-          </div>
-        </div>
-    ),
-  },
-  // {
-  //   url: "https://5kmrun.bg/images/HaderSelfie.png",
-  // },
-  {
-    url: "https://5kmrun.bg/files/header_2_large.jpg",
-    header: "2023-та година с 5kmrun.bg",
-
-  },
-  {
-    url: "https://5kmrun.bg/files/header_3_large.jpg",
-    header: "2023-та година с 5kmrun.bg",
-
-  },
-];
-
 export const StatsPage = ({ userId }) => {
   const [stats, setStats] = useState(null);
 
@@ -67,6 +20,115 @@ export const StatsPage = ({ userId }) => {
   }, [userId]);
 
   if (!stats) return <div>Зареждане...</div>;
+
+  const stories = [
+    {
+      content: (props) => (
+        <div style={{ backgroundImage: "url('https://5kmrun.bg/files/header_5_large.jpg')", backgroundColor: 'rgba(0,0,0,0.5)' }}>
+          <div style={{ backgroundColor: 'rgba(0,0,0,0.5)', padding: "20px" }}>
+            <h1 style={{ marginTop: 5 }}>2023-та с 5kmrun.bg</h1>
+            <div>
+              Отправяме специален поздрав за всеки един участник на 5kmrun през 2023 г. и желаем здраве и още повече пробягани километри през Новата година!
+            </div>
+            <img src={joroAvatar} />
+          </div>
+        </div>
+      ),
+    },
+    {
+      content: (props) => (
+        <div style={{ backgroundImage: "url('https://5kmrun.bg/files/header_2_large.jpg')", backgroundColor: 'rgba(0,0,0,0.5)' }}>
+          <div style={{ backgroundColor: 'rgba(0,0,0,0.5)', padding: "20px" }}>
+            <h1 style={{ marginTop: 5 }}>Същински бягания</h1>
+            <div>
+              През тази година участвахте в {stats?.officialRuns.activeWeeks} бягания.
+            </div>
+            <div>
+              Пробягахте общо {stats?.officialRuns.totalDistance} километра.
+            </div>
+            <div>
+              И това ви отне общо {stats?.officialRuns.totalTime} часа.
+            </div>
+          </div>
+        </div>
+      ),
+    },
+    {
+      content: (props) => (
+        <div style={{ backgroundImage: "url('https://5kmrun.bg/files/header_3_large.jpg')", backgroundColor: 'rgba(0,0,0,0.5)' }}>
+          <div style={{ backgroundColor: 'rgba(0,0,0,0.5)', padding: "20px" }}>
+            <h1 style={{ marginTop: 5 }}>Паркове</h1>
+            <div>
+              Любимият ви парк за бягане през 2023 бе ... с ... бягания.
+            </div>
+            <div>
+              Ето и класацията на парковете, в които сте бягали през 2023:
+            </div>
+          </div>
+        </div>
+      ),
+    },
+    {
+      content: (props) => (
+        <div style={{ backgroundImage: "url('https://5kmrun.bg/images/HaderSelfie.png')", backgroundColor: 'rgba(0,0,0,0.5)' }}>
+          <div style={{ backgroundColor: 'rgba(0,0,0,0.5)', padding: "20px" }}>
+            <h1 style={{ marginTop: 5 }}>Selfie бягания</h1>
+            <div>
+              В Selfie класацията, бяхте активни в {stats?.selfieRuns.activeWeeks} седмици.
+            </div>
+            <div>
+              Пробягахте общо {stats?.selfieRuns.totalDistance} километра.
+            </div>
+            <div>
+              И това ви отне общо {stats?.selfieRuns.totalTime} часа.
+            </div>
+          </div>
+        </div>
+      ),
+    },
+    {
+      content: (props) => (
+        <div style={{ backgroundImage: "url('https://5kmrun.bg/images/HaderSelfie.png')", backgroundColor: 'rgba(0,0,0,0.5)' }}>
+          <div style={{ backgroundColor: 'rgba(0,0,0,0.5)', padding: "20px" }}>
+            <h1 style={{ marginTop: 5 }}>Вашите рекорди</h1>
+            <div>
+              Ето и вашите най-бързи бягания през изминалата година:
+            </div>
+          </div>
+        </div>
+      ),
+    },
+    {
+      content: (props) => (
+        <div style={{ backgroundImage: "url('https://5kmrun.bg/images/HaderSelfie.png')", backgroundColor: 'rgba(0,0,0,0.5)' }}>
+          <div style={{ backgroundColor: 'rgba(0,0,0,0.5)', padding: "20px" }}>
+            <h1 style={{ marginTop: 5 }}>Вашите постижения</h1>
+            <div>
+              Ето и вашите най-добри класирания през 2023 година:
+            </div>
+          </div>
+        </div>
+      ),
+    },
+    {
+      content: (props) => (
+        <div style={{ backgroundImage: "url('https://5kmrun.bg/images/HaderSelfie.png')", backgroundColor: 'rgba(0,0,0,0.5)' }}>
+          <div style={{ backgroundColor: 'rgba(0,0,0,0.5)', padding: "20px" }}>
+            <h1 style={{ marginTop: 5 }}>XLRuns</h1>
+            <div>
+              Брой участия в XLRuns: {stats?.xlRuns.activeWeeks}
+            </div>
+            <div>
+              Пробягани километри: {stats?.xlRuns.totalDistance}
+            </div>
+            <div>
+              Общо време: {stats?.xlRuns.totalTime}
+            </div>
+          </div>
+        </div>
+      ),
+    },
+  ];
 
   return (
     <Stories
