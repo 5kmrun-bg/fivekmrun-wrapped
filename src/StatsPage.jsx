41198f04a2ac0426e280b01c40830587a885a8d0
vakrilov <alexander.vakrilov@gmail.com>
2024-01-08 23:51:09 +0200

    extract share button into component

--- src/StatsPage.jsx
+++ src/StatsPage.jsx
@@ -5,6 +5,7 @@ import { toBlob } from "html-to-image";
 import { loadStats } from "./stats";
 import { createStories } from "./stories";
 import "./StatsPage.scss";
+import { ShareButton } from "./ShareButton";
 
 const PADDING = 16;
 const StatsStyle = { "--stats-padding": `${PADDING}px` };
@@ -75,24 +76,7 @@ export const StatsPage = ({ userId }) => {
         keyboardNavigation
       />
       {showShare && (
-        <button className="share-button" onClick={handleShare}>
-          <svg
-            t="1580465783605"
-            viewBox="0 0 1024 1024"
-            version="1.1"
-            xmlns="http://www.w3.org/2000/svg"
-            p-id="9773"
-            width="30"
-            height="30"
-          >
-            <path
-              d="M767.99994 585.142857q75.995429 0 129.462857 53.394286t53.394286 129.462857-53.394286 129.462857-129.462857 53.394286-129.462857-53.394286-53.394286-129.462857q0-6.875429 1.170286-19.456l-205.677714-102.838857q-52.589714 49.152-124.562286 49.152-75.995429 0-129.462857-53.394286t-53.394286-129.462857 53.394286-129.462857 129.462857-53.394286q71.972571 0 124.562286 49.152l205.677714-102.838857q-1.170286-12.580571-1.170286-19.456 0-75.995429 53.394286-129.462857t129.462857-53.394286 129.462857 53.394286 53.394286 129.462857-53.394286 129.462857-129.462857 53.394286q-71.972571 0-124.562286-49.152l-205.677714 102.838857q1.170286 12.580571 1.170286 19.456t-1.170286 19.456l205.677714 102.838857q52.589714-49.152 124.562286-49.152z"
-              p-id="9774"
-              fill="#ffffff"
-            ></path>
-          </svg>
-          <span>Сподели</span>
-        </button>
+        <ShareButton className="share-button" onClick={handleShare} />
       )}
     </div>
   );
