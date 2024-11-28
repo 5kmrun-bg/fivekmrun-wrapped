80fdb865f1ae31ca7d1d3c48880dd8e87ca0456a
vakrilov <alexander.vakrilov@gmail.com>
2024-11-28 15:30:07 +0200

    add framer motion

--- src/api/constants.ts
+++ src/api/constants.ts
@@ -1,6 +1,6 @@
 export const BASE_URL =
   import.meta.env.MODE === "development"
-    ? "https://192.168.1.6:5173/api/" // TODO: Only for testing on mobile device
+    ? "https://localhost:5173/api/" // TODO: Only for testing on mobile device
     : "https://5kmrun.bg/api/";
 
 export const YEAR = 2024;
